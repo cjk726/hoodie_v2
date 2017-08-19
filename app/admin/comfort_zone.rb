d6efc18521f0d28dba34eb741364ddac2df5c40e
Raghu Betina <raghu@firstdraft.com>
2017-08-19 01:32:36 +0000

    Whitelist columns in ComfortZone for ActiveAdmin

--- app/admin/comfort_zone.rb
+++ app/admin/comfort_zone.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ComfortZone do
 
+ permit_params :comfort_zone_name, :comfort_zone_temp_min
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
