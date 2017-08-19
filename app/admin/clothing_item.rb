90cb5faefd713f439d2a0b11aafcba70e3140697
Raghu Betina <raghu@firstdraft.com>
2017-08-19 01:32:32 +0000

    Whitelist columns in ClothingItem for ActiveAdmin

--- app/admin/clothing_item.rb
+++ app/admin/clothing_item.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ClothingItem do
 
+ permit_params :item_name
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
