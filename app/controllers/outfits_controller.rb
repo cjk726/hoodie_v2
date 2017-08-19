f3632f6533dd34d07bc8a40750c0e60602f22018
Raghu Betina <raghu@firstdraft.com>
2017-08-19 01:32:37 +0000

    Only outfit user can access show page

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -1,5 +1,5 @@
 class OutfitsController < ApplicationController
-  before_action :current_user_must_be_outfit_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_outfit_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_outfit_user
     outfit = Outfit.find(params[:id])
